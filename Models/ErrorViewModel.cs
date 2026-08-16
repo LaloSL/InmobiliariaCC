fae828b494162478008505305f07570e6355509a
LaloSL <127783873+LaloSL@users.noreply.github.com>
2026-08-16 17:12:55 -0300

    Estructura inicial del proyecto ASP.NET MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InmobiliariaCC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
